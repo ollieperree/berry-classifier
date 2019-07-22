9db970b044f71b93f953ee71105032bd29580de8
ollieperree <26961541+ollieperree@users.noreply.github.com>
2019-07-22 20:08:15 +0100

    Change server.py for berries model

--- app/server.py
+++ app/server.py
@@ -9,10 +9,11 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1KqlQ_kOZJe_4VBydNz2TmSE0D-wvg7wN'
+export_file_name = 'resnet_50_berries_1.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['blackberries', 'blueberries', 'cloudberries', 'gooseberries',
+           'grapes', 'raspberries', 'redcurrants', 'strawberries']
 path = Path(__file__).parent
 
 app = Starlette()
